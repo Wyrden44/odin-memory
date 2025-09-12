a6e90e769c80d4067da6ac3f1385aa71ce88ae9d
Pawel Gurlo <pawel.gurlo@gmail.com>
2025-09-12 13:14:56 +0200

    Link difficulty selection with amount of cards and add loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,36 @@ import SelectDifficulty from './components/SelectDifficulty';
 
 function App() {
   const [screen, setScreen] = useState(0);
+  const [difficulty, setDifficulty] = useState("easy");
+
+  function onDifficultySelect(diff) {
+    switch (diff) {
+      case "easy":
+        setDifficulty("easy");
+        break;
+      case "medium":
+        setDifficulty("medium");
+        break;
+      case "hard":
+        setDifficulty("hard");
+        break;
+      default:
+        break;
+    }
+    setScreen(1);
+  }
   
   if (screen == 0) {
     return (
       <>
-        <SelectDifficulty onDifficultySelect={() => setScreen(1)}/>
+        <SelectDifficulty onDifficultySelect={onDifficultySelect}/>
       </>
     )
   }
   else {
     return (
       <>
-        <Game />
+        <Game difficulty={difficulty}/>
       </>
     )
   }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import Board from "./Board";
 import loadPokemonImages from "../hooks/loadPokemonImages.js";
+import Spinner from "./Spinner.jsx";
 
-const IMAGE_COUNT = 10;
-
-export default function Game() {
+export default function Game({ difficulty }) {
     const [score, setScore] = useState(0);
     const [highScore, setHighScore] = useState(0);
     const [images, setImages] = useState([]);
+    const [spinner, setSpinner] = useState(true);
 
     function updateScore(newScore) {
         setScore(newScore);
@@ -22,6 +22,16 @@ export default function Game() {
 
     useEffect(() => {
         async function load() {
+            let IMAGE_COUNT = 5;
+            if (difficulty === "easy") {
+                IMAGE_COUNT = 5;
+            }
+            else if (difficulty === "medium") {
+                IMAGE_COUNT = 10;
+            }
+            else if (difficulty === "hard") {
+                IMAGE_COUNT = 20;
+            }
             // load images when initialising
             const pokemonImages = await loadPokemonImages(IMAGE_COUNT);
 
@@ -29,28 +39,38 @@ export default function Game() {
         }
 
         load();
+        setSpinner(false);
     }, [])
 
-    return (
-        <>
-            <div className="game">
-                <div className="score">
-                    
-                    <div className="score-section">
-                        <p className="label">SCORE</p>
-                        <div className="score-score">
-                            <p className="score-real">{score}</p>
+    if (spinner) {
+        return (
+            <>
+                <Spinner />
+            </>
+        )
+    }
+    else {
+        return (
+            <>
+                <div className="game">
+                    <div className="score">
+                        
+                        <div className="score-section">
+                            <p className="label">SCORE</p>
+                            <div className="score-score">
+                                <p className="score-real">{score}</p>
+                            </div>
+                        </div>
+                        <div className="score-section">
+                            <p className="label">HIGH SCORE</p>
+                            <div className="score-score">
+                                <p className="score-real">{highScore}</p>
+                            </div>
                         </div>
                     </div>
-                    <div className="score-section">
-                        <p className="label">HIGH SCORE</p>
-                        <div className="score-score">
-                            <p className="score-real">{highScore}</p>
-                        </div>
-                    </div>
+                    <Board onGameOver={onGameOver} updateScore={updateScore} images={images} />
                 </div>
-                <Board onGameOver={onGameOver} updateScore={updateScore} images={images} />
-            </div>
-        </>
-    );
+            </>
+        );
+    }
 }
